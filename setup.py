8308c4f4464c85ed464db37d811f714f9cf64655
Tom Gross <itconsense@gmail.com>
2016-03-17 12:18:46 +0000

    specify dependencies

--- setup.py
+++ setup.py
@@ -15,7 +15,10 @@ setup(name='collective.contentrules.mail',
           "Framework :: Plone :: 4.1",
           "Framework :: Plone :: 4.2",
           "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
           ],
       keywords='',
@@ -30,10 +33,14 @@ setup(name='collective.contentrules.mail',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-          'plone.app.kss'
+          'plone.app.kss',
+          'zope.app.form',
+
       ],
       extras_require = {
-        "test": ["plone.app.testing>=4.2.3", ],
+        "test": [
+            "plone.app.testing>=4.2.3",
+            "Products.PloneTestCase"],
       },
       entry_points="""
       [z3c.autoinclude.plugin]
